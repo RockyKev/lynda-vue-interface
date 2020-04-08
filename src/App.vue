966f66aff22fcb517444520e21d957df6a925f57
Rocky Kev <rockykev@Rockys-iMac.lab>
2020-04-07 17:45:54 -0700

    deleted records

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
   <div id="main-app" class="container">
-    <h4>{{ title }}</h4>
-
     <div class="row justify-content-center">
       <font-awesome-icon icon="plus" class="mr-2" />Add Appointment
-      <Appointment-List :appointments="appointments" />
+      <Appointment-List :appointments="appointments" @remove="removeItem" />
     </div>
   </div>
 </template>
 
 <script>
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import _ from "lodash";
 import axios from "axios";
 
 import AppointmentList from "./components/AppointmentList";
@@ -19,7 +18,6 @@ export default {
   name: "MainApp",
   data: function() {
     return {
-      title: "Appointment List!!",
       appointments: []
     };
   },
@@ -32,6 +30,11 @@ export default {
     axios
       .get("./data/appointments.json")
       .then(response => (this.appointments = response.data));
+  },
+  methods: {
+    removeItem: function(apt) {
+      this.appointments = _.without(this.appointments, apt);
+    }
   }
 };
 </script>
